c07ad2c6b98fddb7e33a55bdd7838206cbef757f
Sean Madsen <sean@seanmadsen.com>
2017-01-10 11:02:15 -0700

    adding document-ready function to js

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -1,23 +1,26 @@
+$(function() {
 
-// Automatically scroll the navigation menu to the active element
-//   https://github.com/civicrm/civicrm-dev-docs/issues/21
-$.fn.isFullyWithinViewport = function(){
-    var viewport = {};
-    viewport.top = $(window).scrollTop();
-    viewport.bottom = viewport.top + $(window).height();
-    var bounds = {};
-    bounds.top = this.offset().top;
-    bounds.bottom = bounds.top + this.outerHeight();
-    return ( ! (
-      (bounds.top <= viewport.top) ||
-      (bounds.bottom >= viewport.bottom)
-    ) );
-};
-if( !$('li.toctree-l1.current').isFullyWithinViewport() ) {
-  $('.wy-nav-side')
-    .scrollTop(
-      $('li.toctree-l1.current').offset().top -
-      $('.wy-nav-side').offset().top -
-      60
-    );
-}
+  // Automatically scroll the navigation menu to the active element
+  //   https://github.com/civicrm/civicrm-dev-docs/issues/21
+  $.fn.isFullyWithinViewport = function(){
+      var viewport = {};
+      viewport.top = $(window).scrollTop();
+      viewport.bottom = viewport.top + $(window).height();
+      var bounds = {};
+      bounds.top = this.offset().top;
+      bounds.bottom = bounds.top + this.outerHeight();
+      return ( ! (
+        (bounds.top <= viewport.top) ||
+        (bounds.bottom >= viewport.bottom)
+      ) );
+  };
+  if( !$('li.toctree-l1.current').isFullyWithinViewport() ) {
+    $('.wy-nav-side')
+      .scrollTop(
+        $('li.toctree-l1.current').offset().top -
+        $('.wy-nav-side').offset().top -
+        60
+      );
+  }
+
+});
