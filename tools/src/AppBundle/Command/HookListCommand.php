58fcd7f926d62fd6e79727b7e721f156ab8b15d4
Sean Madsen <sean@seanmadsen.com>
2017-06-25 21:07:58 -0600

    List hooks by category

--- tools/src/AppBundle/Command/HookListCommand.php
+++ tools/src/AppBundle/Command/HookListCommand.php
@@ -5,9 +5,16 @@ namespace AppBundle\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Twig_Environment;
+use Twig_Loader_Filesystem;
 use AppBundle\Utils\Book;
 
 class HookListCommand extends Command {
+
+  /*
+   * Data about all hooks, sorted by category
+   */
+  protected $categories = array();
 
   protected function configure() {
     $this
@@ -16,9 +23,59 @@ class HookListCommand extends Command {
   }
 
   protected function execute(InputInterface $input, OutputInterface $output) {
-    $hooks = Book::pages()['Hooks'];
+    $this->loadList();
+    $this->loadHookSummaries();
+    $this->writeOut();
+  }
 
-    //$output->write(var_export($hooks, TRUE));
+  /**
+   * Populate $this->categories with data
+   */
+  protected function loadList() {
+    // Get data from mkdocs.yml file under the 'Hooks' element
+    $categories = Book::pages()['Hooks'];
+
+    // Remove pages which are not about specific hooks
+    $categories = array_map(function ($pages) {
+      if (is_array($pages)) {
+        return array_filter($pages, function ($page_url) {
+          return preg_match('/hook_civicrm_/', $page_url);
+        });
+      }
+      else {
+        return FALSE;
+      }
+    }, $categories);
+
+    // Remove empty categories
+    $categories = array_filter($categories);
+
+    $this->categories = $categories;
+  }
+
+  /**
+   * Write the file with markdown content
+   */
+  protected function writeOut() {
+    $loader = new Twig_Loader_Filesystem(__DIR__ . '/../Resources/views');
+    $twig = new Twig_Environment($loader);
+    $content = htmlspecialchars_decode($twig->render(
+      "hook_list.md.twig",
+      array(
+        'categories' => $this->categories,
+        'command' => './bin/tools ' . $this->getName()
+      )
+    ));
+    file_put_contents(__DIR__ . '/../../../../docs/hooks/list.md', $content);
+  }
+  
+  protected function loadHookSummaries() {
+    foreach ($this->categories as $category => $hooks) {
+
+    }
+  }
+
+  protected function loadHookSummary($hookUrl) {
 
   }
 
